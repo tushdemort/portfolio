73dcc20dae89c9c67b7c02709f2c8dedc40299dd
tushdemort <anand.tushar2010@gmail.com>
2023-08-18 12:00:15 +0530

    yeet

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -12,8 +12,8 @@ export class Cards extends Component {
     <div className='card'>
       <div className="card-body">
         <h5 className="card-title">{this.props.name}</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">View Project</a>
+        <p className="card-text">{this.props.desc}</p>
+        <a href={this.props.link} target='_blank'  className="btn btn-primary">View Project</a>
     </div>
     </div>
     </>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,12 +12,12 @@ export class Projects extends Component {
         <br/>
         <div className='heading' >Projects</div>
         <div class="wrapper">
-          <div><Cards name='WILP'/></div>
-          <div><Cards name='Senitiment Analysis'/></div>
-          <div><Cards name='TechWeek Website'/></div>
-          <div><Cards name='Stockr'/></div>
-          <div><Cards name='Portfolio'/></div>
-          <div><Cards name='Aruco Detector'/></div>
+          <div><Cards name='WILP' desc='Developed a tool for WILP professors to generate, modify and store question. Further these questions are used to generate question papers for students during their Midterms and Endsems.' link='https://google.com'/></div>
+          <div><Cards name='Senitiment Analysis' desc='An app that uses the News api to fetch the latest news articles related to a topic and runs sentiment analysis on the articles to determine the medias opinio on the topic.' link='https://github.com/tushdemort/newsanalyser'/></div>
+          <div><Cards name='TechWeek Website' desc='Official website for TechWeek 23 at BITS Pilani, Hyderabad Campus. Links to all the events on unstop are available on this website along with a gallery of past events .' link='https://techweek-bitsh.com'/> </div>
+          <div><Cards name='Stockr' desc='A python and streamlit app that predicts stock price and can be used to calculate gains that could have been made by investing in a particular stock on the market.' link='https://github.com/tushdemort/stockr'/></div>
+          <div><Cards name='Portfolio' desc='Personal portfolio website made using react for an ACM project. Links to all my current and past projects, shows skills, links to my social media and provides my resume.' link='https://tdm.onrender.com'/></div>
+          <div><Cards name='Aruco Detector' desc='A python script to detect arUco markers, their orientation, draw a border around them and read their IDs. Useful in building autonomous rovers and drones.'/></div>
         </div>
       </>
     )
